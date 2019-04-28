0403b039b2a25b83c279c418d61b6a1db1e1d75b
Juliana Karoline <julianaklulo@gmail.com>
2019-04-27 23:56:37 -0300

    Adiciona a criação e edição de usuários

--- PromocaoIngressos/src/java/br/ufscar/dc/dsw/dao/GenericDAO.java
+++ PromocaoIngressos/src/java/br/ufscar/dc/dsw/dao/GenericDAO.java
@@ -1,10 +1,11 @@
 package br.ufscar.dc.dsw.dao;
+import br.ufscar.dc.dsw.model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
 
 abstract public class GenericDAO {
     public GenericDAO() {
@@ -16,6 +17,38 @@ abstract public class GenericDAO {
     }
     
     protected Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:derby://localhost:1527/SiteVenda", "root", "root");
+        return DriverManager.getConnection("jdbc:derby://localhost:1527/PromocaoIngressos", "root", "root");
+    }
+    
+    public void insertUsuario(Usuario usuario) {
+        String sql = "INSERT INTO usuarios (email, senha, papel, ativo) VALUES (?, ?, ?, true)";
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, usuario.getEmail());
+            statement.setString(2, usuario.getSenha());
+            statement.setString(3, usuario.getPapel());
+            statement.executeUpdate();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+        throw new RuntimeException(e);
+        }
+    }
+    
+    public void updateUsuario(Usuario usuario) {
+        String sql = "UPDATE usuarios SET senha = ?";
+        sql += " WHERE email = ?";
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, usuario.getSenha());
+            statement.setString(2, usuario.getEmail());
+            statement.executeUpdate();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+        throw new RuntimeException(e);
+        }
     }
 }
