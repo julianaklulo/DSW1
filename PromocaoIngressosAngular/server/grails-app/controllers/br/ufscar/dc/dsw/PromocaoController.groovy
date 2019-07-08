c75d9943f17e15ce28498acafa0559e7fa375e82
julianaklulo <julianaklulo@gmail.com>
2019-07-07 23:40:14 -0300

    Listagem de Promoções (R7) - backend

--- PromocaoIngressosAngular/server/grails-app/controllers/br/ufscar/dc/dsw/PromocaoController.groovy
+++ PromocaoIngressosAngular/server/grails-app/controllers/br/ufscar/dc/dsw/PromocaoController.groovy
@@ -4,10 +4,39 @@ import grails.rest.*
 import grails.converters.*
 import grails.plugin.springsecurity.annotation.Secured
 
-@Secured(['ROLE_TEATRO'])
 class PromocaoController extends RestfulController {
     static responseFormats = ['json', 'xml']
     PromocaoController() {
         super(Promocao)
     }
+    @Secured(['ROLE_SITE'])
+    @Override
+    def index(Integer max) {
+        super.index(max)
+    }
+
+    @Secured(['ROLE_SITE'])
+    @Override
+    def show() {
+        super.show()
+    }
+
+    @Secured(['ROLE_TEATRO'])
+    @Override
+    def save() {
+        super.save()
+    }
+
+    @Secured(['ROLE_TEATRO'])
+    @Override
+    def update() {
+        super.update()
+    }
+
+    @Secured(['ROLE_TEATRO'])
+    @Override
+    def delete() {
+        super.delete()
+    }
+
 }
