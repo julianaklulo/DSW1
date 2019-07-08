05284b654828de466ded621ea0b17af40ec3f260
julianaklulo <julianaklulo@gmail.com>
2019-07-07 23:39:12 -0300

    Listagem de Teatros (R3) - backend

--- PromocaoIngressosAngular/server/grails-app/controllers/br/ufscar/dc/dsw/TeatroController.groovy
+++ PromocaoIngressosAngular/server/grails-app/controllers/br/ufscar/dc/dsw/TeatroController.groovy
@@ -4,10 +4,38 @@ import grails.rest.*
 import grails.converters.*
 import grails.plugin.springsecurity.annotation.Secured
 
-@Secured(['ROLE_ADMIN'])
 class TeatroController extends RestfulController {
     static responseFormats = ['json', 'xml']
     TeatroController() {
         super(Teatro)
+    }
+    @Secured(['permitAll()'])
+    @Override
+    def index(Integer max) {
+        super.index(max)
+    }
+
+    @Secured(['permitAll()'])
+    @Override
+    def show() {
+        super.show()
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    @Override
+    def save() {
+        super.save()
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    @Override
+    def update() {
+        super.update()
+    }
+
+    @Secured(['ROLE_ADMIN'])
+    @Override
+    def delete() {
+        super.delete()
     }
 }
